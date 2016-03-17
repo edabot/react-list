343d72495367ba23665cdcca4b0f6d9e0e3ae111
edabot <ed.lewis@gmail.com>
2016-03-16 21:24:50 -0700

    disable text edit for finished items

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -24,6 +24,7 @@ module.exports = React.createClass({
         onChange={this.handleDoneChange} />
       </span>
       <input type="text"
+      disabled={this.state.done}
         className="form-control"
         onChange={this.handleTextChange}
         value={this.state.text}
@@ -42,11 +43,27 @@ module.exports = React.createClass({
     if (!this.state.textChanged){
       return null
     } else {
-      return <span>
-        <button className="btn btn-default">Save</button>
-        <button className="btn btn-default">Undo</button>
-      </span>
+      return [
+        <button
+          className="btn btn-default"
+          onClick={this.handleSaveClick}>
+          Save</button>,
+        <button
+          className="btn btn-default"
+          onClick={this.handleUndoClick}>
+        Undo</button>
+      ]
     }
+  },
+  handleSaveClick: function() {
+    this.fb.update({text: this.state.text});
+    this.setState({textChanged: false});
+  },
+  handleUndoClick: function() {
+    this.setState({
+      text: this.props.item.text,
+      textChanged: false
+    });
   },
   handleDoneChange: function(event) {
     var update = {done: event.target.checked}
