9eca9fae39f8d745132a91e5457a738c844cccea
edabot <ed.lewis@gmail.com>
2016-03-16 20:58:26 -0700

    make checkbox work with firebase

--- src/list-item.jsx
+++ src/list-item.jsx
@@ -1,15 +1,26 @@
 var React = require('react');
+var ReactFire = require('reactfire');
+var FireBase = require('firebase');
+var rootUrl = 'https://scorching-fire-8932.firebaseio.com/';
+
 
 module.exports = React.createClass({
   getInitialState: function() {
     return {
-      text: this.props.item.text
+      text: this.props.item.text,
+      done: this.props.item.done
     }
+  },
+  componentWillMount: function() {
+    this.fb = new Firebase(rootUrl + "items/" + this.props.item.key)
   },
   render: function () {
     return <div className="input-group">
       <span className="input-group-addon">
-        <input type="checkbox" />
+        <input
+        type="checkbox"
+        checked={this.state.done}
+        onChange={this.handleDoneChange} />
       </span>
       <input type="text"
         className="form-control"
@@ -21,6 +32,12 @@ module.exports = React.createClass({
         </button>
       </span>
     </div>
+  },
+
+  handleDoneChange: function(event) {
+    var update = {done: event.target.checked}
+    this.setState(update);
+    this.fb.update(update);
   }
 
 });
